f8d22fc9026e84f2cd40a759d838a1910b177515
nutzipper <1746367+nzpr@users.noreply.github.com>
2023-11-27 20:14:31 +0400

    Multiarch CI

--- compile.sh
+++ compile.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+# To cross compile to aarch64 use "arm" as a first argument
+# Example:
+#   ./compile.sh arm
+ARCH_HOST="$1"
+echo "Arch host = '$ARCH_HOST'"
+
+rm -Rf secp256k1-tmp/
 cp -r secp256k1/ secp256k1-tmp/
 
 pushd secp256k1-tmp/
@@ -16,8 +23,20 @@ cp ../jni/configure.ac configure.ac
 
 # Compile secp256k1 native code
 ./autogen.sh
-./configure --enable-jni --enable-module-ecdh --enable-experimental --enable-module-schnorrsig --enable-module-ecdsa-adaptor
+
+if [[ "$ARCH_HOST" ]]; then
+  echo "Cross compiling for ARM CPU... $ARCH_HOST"
+  # ./configure --enable-jni --enable-module-ecdh --host=aarch64-linux-gnu
+  # ./configure --enable-jni --enable-module-ecdh --host=aarch64-linux-gnu --enable-experimental --with-asm=arm
+  # ./configure --enable-jni --enable-module-ecdh --host=aarch64-linux-gnu --with-asm=no
+  ./configure --enable-jni --enable-module-ecdh --host=$ARCH_HOST --with-asm=no
+else
+  ./configure --enable-jni --enable-module-ecdh
+fi
+# ./configure --enable-jni --enable-module-ecdh --enable-experimental --enable-module-schnorrsig
 # ./configure --enable-jni --enable-module-ecdh
 make CFLAGS="-std=c99"
 make
-make check
+if [[ ! "$ARCH_HOST" ]]; then
+  make check
+fi
